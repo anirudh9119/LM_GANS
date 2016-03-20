7f31bb9f072e02d06da49f3e59ea6f13c2cb83a4
Alex Lamb <lambalex@leto50.iro.umontreal.ca>
2016-03-20 05:15:46 -0400

    changed controller log location to logs folder

--- lm_controller.py
+++ lm_controller.py
@@ -52,7 +52,7 @@ class LMController(Controller):
         self._stop = False
 
         self.experiment_id = experiment_id
-        ServerLogger(filename='{}.log.jsonl.gz'.format(self.experiment_id),
+        ServerLogger(filename='logs/{}.log.jsonl.gz'.format(self.experiment_id),
                      threaded=True, port=config['multi']['log_port'])
 
 
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     #num_workers = int(sys.argv[2])
     # Create unique experiment ID and backup config file
     experiment_id = binascii.hexlify(os.urandom(3)).decode()
-    shutil.copyfile(sys.argv[1], '{}.config.json'.format(experiment_id))
+    shutil.copyfile(sys.argv[1], 'logs/{}.config.json'.format(experiment_id))
     # Start controller
     l = LMController(experiment_id, config)
     l.start_batch_server()
